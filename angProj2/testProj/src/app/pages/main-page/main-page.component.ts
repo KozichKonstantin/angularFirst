56309c5ddd8d70710f2e081ccc902ee931fc2815
KozichKonstantin <humanstepyt@gmail.com>
2023-10-13 00:58:51 +0300

    connected search with button

--- angProj2/testProj/src/app/pages/main-page/main-page.component.ts
+++ angProj2/testProj/src/app/pages/main-page/main-page.component.ts
@@ -10,13 +10,18 @@ import { SearchComponent } from 'src/app/components/search/search.component';
 export class MainPageComponent implements OnInit {
   products: IProduct[] = [];
   public term : string;
+  public searchValue: string;
   public page : number;
   public pageSize : number;
   public collectionSize : number;
+  changeTerm(searchValue = ''): void{
+    this.term = searchValue;
+  }
   constructor(private productsService: ProductsService) {
     this.page = 1;
     this.pageSize = 4;
-    this.term =''
+    this.term ='';
+    this.changeTerm(this.term)
   }
   ngOnInit(): void {
     this.productsService.getQ().subscribe((products) => {
