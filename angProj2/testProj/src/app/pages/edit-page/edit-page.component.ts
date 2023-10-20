d176137a4462cafc06cb5b701033c26a340f6b08
KozichKonstantin <humanstepyt@gmail.com>
2023-10-20 23:22:47 +0300

    20.10.2023 V2.5d
    editions in edit-page
    added some logic

--- angProj2/testProj/src/app/pages/edit-page/edit-page.component.ts
+++ angProj2/testProj/src/app/pages/edit-page/edit-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.css']
 })
 export class EditPageComponent {
+  constructor(
+    private router: Router){
 
+  }
+  post(){
+    console.log('posted lol')
+  }
+  goHome(value: string = ''){
+    if(value=='post'){
+      console.log('like posted')
+      this.router.navigate(['main'])
+    }else{
+      this.router.navigate(['main'])
+    }
+  }
 }
