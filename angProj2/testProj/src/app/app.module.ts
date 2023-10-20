a0eb67136e4859ac5b36fbd835e4b6bbdc284678
KozichKonstantin <humanstepyt@gmail.com>
2023-10-20 23:24:57 +0300

    20.10.2023 V2.5
    fixed one issue

--- angProj2/testProj/src/app/app.module.ts
+++ angProj2/testProj/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
-import LoginPageComponent from './pages/login-page/login-page.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { EditPageComponent } from './pages/edit-page/edit-page.component';
 import { SearchComponent } from './components/search/search.component';
