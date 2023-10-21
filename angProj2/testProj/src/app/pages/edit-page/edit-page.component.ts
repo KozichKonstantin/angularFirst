141dbd37ecb14fda969d943eeaaf926d2f01074a
KozichKonstantin <humanstepyt@gmail.com>
2023-10-21 21:44:31 +0300

    21.10.2023 V2.5.1
    wrote first version of saving logic

--- angProj2/testProj/src/app/pages/edit-page/edit-page.component.ts
+++ angProj2/testProj/src/app/pages/edit-page/edit-page.component.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { IProduct } from 'src/app/models/prod';
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.css']
 })
 export class EditPageComponent {
+  public nameInp: string;
+  public modelInp: string;
+  public numberMin: string;
+  public numberMax: string;
+  public typeSelect: string;
+  public unitSelect: string;
+  public locationInp: string;
+
   constructor(
     private router: Router){
 
   }
   post(){
     console.log('posted lol')
+  }
+  saveEl(a: string, b: string, c: string, d: string, e: string, f: string, g: string){
+    const newProd: IProduct = {
+      name :a,
+      model : b,
+      type : c,
+      range : (d + ' ' + e),
+      unit: f,
+      location: g
+    }
+    return console.log(newProd)
+
   }
   goHome(value: string = ''){
     if(value=='post'){
